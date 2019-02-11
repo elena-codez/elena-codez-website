6e1e7b5e030c524e79aeb71e7ab7c9139f6f846d
David Amos <somacdivad@gmail.com>
2019-02-11 06:52:48 -0600

    Fix MIDDLEWARE in staging

--- elena_codez/config/settings/staging.py
+++ elena_codez/config/settings/staging.py
@@ -6,7 +6,7 @@ from .base import *  # noqa: F401, F403
 
 DEBUG = False  # noqa: F405
 
-MIDDLEWARE += "whitenoise.middleware.WhiteNoiseMiddleware"  # noqa: F405
+MIDDLEWARE += ["whitenoise.middleware.WhiteNoiseMiddleware"]  # noqa: F405
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # noqa: F405
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
