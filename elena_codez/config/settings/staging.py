7aa68b0df603109316a4a65e1845d731f2a3a932
David Amos <somacdivad@gmail.com>
2019-02-11 06:05:15 -0600

    Add noqa to pass linter

--- elena_codez/config/settings/staging.py
+++ elena_codez/config/settings/staging.py
@@ -1,7 +1,9 @@
+import os
+
 import django_heroku
 
 from .base import *  # noqa: F401, F403
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # noqa: F405
 
 django_heroku.settings(locals())
