78990378b2ce0535a1ca3ac32f132e013d4c9e58
David Amos <somacdivad@gmail.com>
2019-02-11 06:00:30 -0600

    Split dev and staging static files dirs

--- elena_codez/config/settings/base.py
+++ elena_codez/config/settings/base.py
@@ -105,6 +105,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticdev")
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "elena_codez/static"), os.path.join(BASE_DIR, "elena_codez/core/static")]
--- elena_codez/config/settings/staging.py
+++ elena_codez/config/settings/staging.py
@@ -2,4 +2,6 @@ import django_heroku
 
 from .base import *  # noqa: F401, F403
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 django_heroku.settings(locals())
