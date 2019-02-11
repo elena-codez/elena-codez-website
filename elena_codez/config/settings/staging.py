66d92336661b16e88e67e033faea9ff07168b621
David Amos <somacdivad@gmail.com>
2019-02-11 06:32:12 -0600

    Remobe DEBUG=True

--- elena_codez/config/settings/staging.py
+++ elena_codez/config/settings/staging.py
@@ -4,6 +4,8 @@ import django_heroku
 
 from .base import *  # noqa: F401, F403
 
+DEBUG = False  # noqa: F405
+
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # noqa: F405
 
 django_heroku.settings(locals())
